8968a5c3b55ae84de6ee6716d06c0cb2e8693277
Telem Tobi <telem16@gmail.com>
2024-10-25 08:17:33 +0300

    Initial Commit

---
+++ TimeAway/TimeAwayApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeAwayApp.swift
+//  TimeAway
+//
+//  Created by Telem Tobi on 25/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct TimeAwayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TimeAwayTests/TimeAwayTests.swift
@@ -0,0 +1,16 @@
+//
+//  TimeAwayTests.swift
+//  TimeAwayTests
+//
+//  Created by Telem Tobi on 25/10/2024.
+//
+
+import Testing
+
+struct TimeAwayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
