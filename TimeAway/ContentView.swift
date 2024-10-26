1688d0412ea45c7d48580b6d0d38c700d7c57096
Telem Tobi <telem16@gmail.com>
2024-10-26 09:35:49 +0300

    Update ContentView.swift

--- TimeAway/ContentView.swift
+++ TimeAway/ContentView.swift
@@ -9,13 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            Color.clear
+                .tabItem {
+                    Label("Home", systemImage: "house")
+                }
+            
+            Color.clear
+                .tabItem {
+                    Label("Plan", systemImage: "globe.europe.africa")
+                }
+            
+            Color.clear
+                .tabItem {
+                    Label("Profile", systemImage: "person")
+                }
         }
-        .padding()
     }
 }
 
